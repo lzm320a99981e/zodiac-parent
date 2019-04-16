90315684ecd9c0625c2123d6a59d64e92f76adfd
zhangguangyong <1243610991@qq.com>
2019-04-16 16:09:09 +0800

    tools

---
+++ zodiac-tools/src/main/java/com/github/lzm320a99981e/zodiac/tools/XMLHelper.java
@@ -0,0 +1,4 @@
+package com.github.lzm320a99981e.zodiac.tools;
+
+public class XMLHelper {
+}
---
+++ zodiac-tools/src/test/java/com/github/lzm320a99981e/zodiac/tools/Tests.java
@@ -0,0 +1,10 @@
+package com.github.lzm320a99981e.zodiac.tools;
+
+import org.junit.Test;
+
+public class Tests {
+    @Test
+    public void test() {
+
+    }
+}
