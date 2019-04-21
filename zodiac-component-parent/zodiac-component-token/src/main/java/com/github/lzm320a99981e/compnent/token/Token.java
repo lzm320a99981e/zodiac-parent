e695b9e0bde270fd6aa92979a44febd7f3af1239
lzm320a99981e <lzm320a99981e@gmail.com>
2019-04-21 11:37:28 +0800

    令牌组件02

--- zodiac-component-parent/zodiac-component-token/src/main/java/com/github/lzm320a99981e/compnent/token/Token.java
+++ zodiac-component-parent/zodiac-component-token/src/main/java/com/github/lzm320a99981e/compnent/token/Token.java
@@ -2,12 +2,21 @@ package com.github.lzm320a99981e.compnent.token;
 
 import lombok.Data;
 
-import java.util.Map;
-
+/**
+ * 令牌数据
+ */
 @Data
 public class Token {
+    /**
+     * 访问令牌
+     */
     private String accessToken;
+    /**
+     * 刷新令牌，当访问令牌过期的时候，可用此令牌去生成一个新的访问令牌（此令牌的有效时间一般会大雨访问令牌的有效时间）
+     */
     private String refreshToken;
+    /**
+     * 访问令牌有效时间（单位：秒）
+     */
     private Integer expiresIn;
-    private Map<String, Object> body;
 }
--- zodiac-component-parent/zodiac-component-token/src/main/java/com/github/lzm320a99981e/compnent/token/TokenManager.java
+++ zodiac-component-parent/zodiac-component-token/src/main/java/com/github/lzm320a99981e/compnent/token/TokenManager.java
@@ -1,16 +1,29 @@
 package com.github.lzm320a99981e.compnent.token;
 
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.github.lzm320a99981e.zodiac.tools.IdGenerator;
+import com.google.common.base.Preconditions;
+import io.jsonwebtoken.*;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 
-import java.util.Date;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
+/**
+ * 令牌管理
+ */
 @Slf4j
 public class TokenManager {
+    /**
+     * 标识用户生成令牌时所传入的参数
+     */
     private static final String TOKEN_BODY_KEY = "BODY";
-    private static final String ACCESS_TOKEN_MARK_KEY = "__ACCESS_TOKEN_MARK_KEY__";
+
+    /**
+     * 标识令牌为一个刷新令牌
+     */
     private static final String REFRESH_TOKEN_MARK_KEY = "__REFRESH_TOKEN_MARK_KEY__";
 
     /**
@@ -45,39 +58,99 @@ public class TokenManager {
         this.base64EncodedSecretKey = base64EncodedSecretKey;
     }
 
+    public Token generate() {
+        return generate(Collections.emptyMap());
+    }
+
     /**
-     * 生成Token
+     * 生成令牌
      *
      * @param body
      * @return
      */
-    public Token generate(Date expiration, Map<String, Object> body) {
-//        Preconditions.checkNotNull(expiration);
-//        Preconditions.checkNotNull(body);
-//        Token.TokenBuilder builder = Token.builder();
-//        // body
-//        builder.body(body);
-//        // id
-//        String id = UUID.randomUUID().toString();
-//        builder.id(id);
-//        // 创建时间
-//        Date creation = new Date();
-//        builder.keyCreation(creation);
-//        // 过期时间
-//        builder.keyExpiration(expiration);
-//        // 生成 key
-//        builder.key(
-//                Jwts.builder()
-//                        .setId(id)
-//                        .claim(TOKEN_BODY_KEY, body)
-//                        .signWith(this.signatureAlgorithm, this.base64EncodedSecretKey)
-//                        .setIssuedAt(creation)
-//                        .setExpiration(expiration)
-//                        .compact()
-//        );
-//        return builder.build();
-        return null;
+    public Token generate(Map<String, Object> body) {
+        Preconditions.checkNotNull(body);
+        Token token = new Token();
+        // 访问令牌
+        builder(token, body, true);
+        // 刷新令牌
+        builder(token, body, false);
+        return token;
     }
 
+    /**
+     * 刷新令牌
+     *
+     * @param refreshToken
+     * @return
+     */
+    public Token refresh(String refreshToken) {
+        Claims claims = parse(refreshToken);
+        if (!claims.containsKey(REFRESH_TOKEN_MARK_KEY)) {
+            throw new TokenException("此令牌不符合刷新令牌规范，请核对后再使用", TokenException.Type.INVALID);
+        }
 
+        Token token = new Token();
+        builder(token, (Map<String, Object>) claims.get(TOKEN_BODY_KEY), true);
+        token.setRefreshToken(refreshToken);
+
+        return token;
+    }
+
+    /**
+     * 校验令牌
+     *
+     * @param token
+     * @return
+     */
+    public Map<String, Object> verify(String token) {
+        return (Map<String, Object>) parse(token).get(TOKEN_BODY_KEY);
+    }
+
+    /**
+     * 解析令牌
+     *
+     * @param token
+     * @return
+     */
+    private Claims parse(String token) {
+        try {
+            Jwt jwt = Jwts.parser().setSigningKey(this.base64EncodedSecretKey).parse(token);
+            return (Claims) jwt.getBody();
+        } catch (Exception e) {
+            if (ExpiredJwtException.class.isAssignableFrom(e.getClass())) {
+                throw new TokenException(e, TokenException.Type.EXPIRED);
+            }
+            throw new TokenException(e, TokenException.Type.INVALID);
+        }
+    }
+
+    /**
+     * 构建令牌
+     *
+     * @param token
+     * @param body
+     * @param access
+     */
+    private void builder(Token token, Map<String, Object> body, boolean access) {
+        DateTime now = DateTime.now();
+        // 令牌构建
+        JwtBuilder builder = Jwts.builder().setId(IdGenerator.uuid32()).setIssuedAt(now.toDate()).signWith(this.signatureAlgorithm, this.base64EncodedSecretKey);
+        // 令牌携带数据体
+        builder.claim(TOKEN_BODY_KEY, body);
+        // 有效时间
+        Integer expiresIn = access ? this.accessTokenDurationInSeconds : this.refreshTokenDurationInSeconds;
+        if (Objects.nonNull(expiresIn)) {
+            builder.setExpiration(now.plusSeconds(expiresIn).toDate());
+        }
+        // 访问令牌
+        if (access) {
+            token.setExpiresIn(expiresIn);
+            token.setAccessToken(builder.compact());
+            return;
+        }
+        // 刷新令牌
+        builder.claim(REFRESH_TOKEN_MARK_KEY, true);
+        token.setRefreshToken(builder.compact());
+    }
 }
