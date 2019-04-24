31f21f752996edf5fd2819c42ba7f0a22c8bf774
zhangguangyong <1243610991@qq.com>
2019-04-24 11:50:12 +0800

    微服务-注册中心02

---
+++ zodiac-cloud-parent/zodiac-cloud-registration/src/main/java/com/github/lzm320a99981e/cloud/registration/ZodiacCloudRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.lzm320a99981e.cloud.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacCloudRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZodiacCloudRegistrationApplication.class, args);
+    }
+
+}
