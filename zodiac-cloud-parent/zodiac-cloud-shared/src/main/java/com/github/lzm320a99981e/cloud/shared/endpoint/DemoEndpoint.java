221100f2f609197a64160e048f2d1575de123211
zhangguangyong <1243610991@qq.com>
2019-05-16 18:46:08 +0800

    示例接口02

--- zodiac-cloud-parent/zodiac-cloud-shared/src/main/java/com/github/lzm320a99981e/cloud/shared/endpoint/DemoEndpoint.java
+++ zodiac-cloud-parent/zodiac-cloud-shared/src/main/java/com/github/lzm320a99981e/cloud/shared/endpoint/DemoEndpoint.java
@@ -1,14 +1,13 @@
 package com.github.lzm320a99981e.cloud.shared.endpoint;
 
+import com.alibaba.fastjson.JSON;
 import com.github.lzm320a99981e.component.token.Token;
 import com.github.lzm320a99981e.component.token.TokenManager;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -28,6 +27,19 @@ public class DemoEndpoint {
     @PostMapping("user/info")
     public Map<String, Object> userInfo(@RequestBody UserInfoRequest request) {
         return tokenManager.verify(request.getAccessToken());
+    }
+
+    @ApiOperation("get测试")
+    @GetMapping("/api/reg")
+    @ResponseBody
+    public Map<String, Object> reg(UserRegRequest request) {
+        return JSON.parseObject(JSON.toJSONString(request));
+    }
+
+    @Data
+    public static class UserRegRequest {
+        private String username;
+        private String password;
     }
 
     @Data
