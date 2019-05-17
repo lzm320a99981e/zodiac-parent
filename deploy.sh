5af87f5770de32366b9679024acbcb6ba4bb2eae
lzm320a99981e <lzm320a99981e@gmail.com>
2019-05-17 23:33:31 +0800

    部署脚本01

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,18 @@
 current_dir=$(cd `dirname $0`; pwd)
 echo "current dir: $current_dir"
 
-# 更新项目 ++++++++++++++++++++++++++++++++++
+# 1.更新项目 ++++++++++++++++++++++++++++++++++
 echo "git pull origin master"
 v_update_result=`git pull origin master`
 echo "git pull result -> $v_update_result"
 
-# 判断是否需要重启 ++++++++++++++++++++++++++++++++++
-# 1.项目是否更新 ++++++++++++++++++++++++++++++++++
 if [[ $(echo ${v_update_result} | grep "Already up-to-date") != "" ]]
 then
     echo "项目没有更新"
     exit 2
 fi
 
-# 2.获取需要升级的模块 ++++++++++++++++++++++++++++++++++
+# 2.获取需要升级的项目 ++++++++++++++++++++++++++++++++++
 declare -A v_module_map=()
 v_module_map["ZODIAC-CLOUD-REGISTRATION-RELEASE-TEST.VERSION"]="zodiac-cloud-parent/zodiac-cloud-registration/target/zodiac-cloud-registration-1.0.jar"
 v_module_map["ZODIAC-CLOUD-SHARED-RELEASE-TEST.VERSION"]="zodiac-cloud-parent/zodiac-cloud-shared/target/zodiac-cloud-shared-1.0.jar"
@@ -31,7 +29,27 @@ do
     fi
 done
 
+# 3.停止项目
 for module in ${v_modules[@]}
 do
-    echo "x-> "${module}
+    v_file_path="${current_dir}/${module}"
+    v_pid=`ps -ef | grep ${v_file_path} | grep -v grep | grep -v kill | awk '{print $2}'`
+    if [[ ${v_pid} ]]; then
+        echo "kill -9 $v_pid -> ($v_file_path)"
+        kill -9 ${v_pid}
+    fi
+done
+
+
+# 4.打包项目 ++++++++++++++++++++++++++++++++++
+echo "mvn clean package -e -U -DskipTests"
+mvn clean package -e -U -DskipTests -Ptest
+
+# 5.启动项目 ++++++++++++++++++++++++++++++++++
+for module in ${v_modules[@]}
+do
+    v_file_path="${current_dir}/${module}"
+    v_config="--spring.profiles.active=test"
+    echo "nohup java -jar -Xms384m -Xmx384m ${v_file_path} > /dev/null 2>&1 &"
+    nohup java -jar -Xms384m -Xmx384m ${v_file_path} ${v_config} > /dev/null 2>&1 &
 done
