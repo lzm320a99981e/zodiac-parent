64b9dfb2fd121ed41013a68edd00c13abc720aad
lzm320a99981e <lzm320a99981e@gmail.com>
2019-05-24 23:40:47 +0800

    1. 发布1.1版本

--- zodiac-app-parent/zodiac-app-gsps/src/main/java/com/zodiac/app/gsps/endpoint/common/PublicEndpoint.java
+++ zodiac-app-parent/zodiac-app-gsps/src/main/java/com/zodiac/app/gsps/endpoint/common/PublicEndpoint.java
@@ -2,10 +2,7 @@ package com.zodiac.app.gsps.endpoint.common;
 
 import com.alibaba.fastjson.JSON;
 import com.github.lzm320a99981e.quickly.starter.Constants;
-import com.github.lzm320a99981e.quickly.starter.cache.CacheRedisHelper;
 import com.zodiac.app.gsps.endpoint.common.dto.PersonalLoginRequest;
-import com.zodiac.app.gsps.endpoint.common.dto.PersonalLoginResponse;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.time.Duration;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("${" + Constants.ENV_PREFIX + "api.router.public-prefix:/public}")
@@ -45,17 +40,4 @@ public class PublicEndpoint {
         return (Map) JSON.toJSON(request);
     }
 
-    @PostMapping("cache-test3")
-    public PersonalLoginResponse test3(@RequestBody PersonalLoginRequest request) {
-        String key = "test03";
-        final PersonalLoginResponse cacheValue = CacheRedisHelper.get(key, PersonalLoginResponse.class);
-        if (Objects.nonNull(cacheValue)) {
-            return cacheValue;
-        }
-
-        final PersonalLoginResponse response = new PersonalLoginResponse();
-        BeanUtils.copyProperties(request, response);
-        CacheRedisHelper.set("test03", request, Duration.ofMinutes(1));
-        return response;
-    }
 }
