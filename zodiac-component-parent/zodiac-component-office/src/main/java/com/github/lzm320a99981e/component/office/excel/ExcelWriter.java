80da2bb9e82fef4f88e0215287eb79dd58b1d2ed
lzm320a99981e <lzm320a99981e@gmail.com>
2019-05-26 14:00:01 +0800

    1. excel reader 08 添加导出时单元格合并处理

--- zodiac-component-parent/zodiac-component-office/src/main/java/com/github/lzm320a99981e/component/office/excel/ExcelWriter.java
+++ zodiac-component-parent/zodiac-component-office/src/main/java/com/github/lzm320a99981e/component/office/excel/ExcelWriter.java
@@ -16,8 +16,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 /**
@@ -29,26 +33,93 @@ public class ExcelWriter {
         return new ExcelWriter();
     }
 
+    /**
+     * 表格数据
+     */
     private Map<String, TableDataEntry> tableDataMap = new HashMap<>();
+    /**
+     * 单元格数据
+     */
     private Map<String, PointDataEntry> pointDataMap = new HashMap<>();
+    /**
+     * 表格单元格合并区域
+     */
+    private Map<Table, List<TableCellMergeRange>> tableCellMergeRangesMap = new HashMap<>();
 
-    public ExcelWriter addTable(Table table, Collection<Map<String, Object>> data) {
+    /**
+     * 添加表格
+     *
+     * @param table
+     * @param data
+     * @return
+     */
+    public ExcelWriter addTable(Table table, List<Map<String, Object>> data) {
         Preconditions.checkState(!tableDataMap.containsKey(table.getDataKey()), "已存在的 dataKey -> %s", table.getDataKey());
         this.tableDataMap.put(table.getDataKey(), new TableDataEntry(table, data));
         return this;
     }
 
+    /**
+     * 添加表格
+     *
+     * @param table
+     * @param data
+     * @param strategy
+     * @return
+     */
+    public ExcelWriter addTable(Table table, List<Map<String, Object>> data, TableCellMergeStrategy strategy) {
+        Preconditions.checkNotNull(strategy);
+        addTable(table, data);
+        this.tableCellMergeRangesMap.put(table, strategy.findCellMergeRanges(data));
+        return this;
+    }
+
+    /**
+     * 添加表格
+     *
+     * @param table
+     * @param data
+     * @param tableCellMergeRanges
+     * @return
+     */
+    public ExcelWriter addTable(Table table, List<Map<String, Object>> data, List<TableCellMergeRange> tableCellMergeRanges) {
+        Preconditions.checkNotNull(tableCellMergeRanges);
+        addTable(table, data);
+        this.tableCellMergeRangesMap.put(table, tableCellMergeRanges);
+        return this;
+    }
+
+    /**
+     * 添加单元格
+     *
+     * @param point
+     * @param data
+     * @return
+     */
     public ExcelWriter addPoint(Point point, Object data) {
         Preconditions.checkState(!tableDataMap.containsKey(point.getDataKey()), "已存在的 dataKey -> %s", point.getDataKey());
         this.pointDataMap.put(point.getDataKey(), new PointDataEntry(point, data));
         return this;
     }
 
-    public ExcelWriter addPoints(Collection<Point> points, Map<String, Object> data) {
+    /**
+     * 添加单元格
+     *
+     * @param points
+     * @param data
+     * @return
+     */
+    public ExcelWriter addPoints(List<Point> points, Map<String, Object> data) {
         points.stream().forEach(item -> this.addPoint(item, data.get(item.getDataKey())));
         return this;
     }
 
+    /**
+     * 根据模板写入
+     *
+     * @param template
+     * @return
+     */
     public byte[] write(InputStream template) {
         if (tableDataMap.isEmpty() && pointDataMap.isEmpty()) {
             throw new RuntimeException("未添加任何数据，请添加数据后操作");
@@ -57,11 +128,11 @@ public class ExcelWriter {
             // 创建工作本
             Workbook workbook = WorkbookFactory.create(template);
 
-            // 表格数据写入
-            tableDataMap.values().forEach(item -> writeTable(workbook, item));
+            // 写入表格数据
+            this.tableDataMap.values().forEach(item -> writeTable(workbook, item));
 
-            // 单元格数据写入
-            pointDataMap.values().forEach(item -> writePoint(workbook, item));
+            // 写入单元格数据
+            this.pointDataMap.values().forEach(item -> writePoint(workbook, item));
 
             // 获取写入后的数据
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -73,6 +144,12 @@ public class ExcelWriter {
         }
     }
 
+    /**
+     * 根据模板写入
+     *
+     * @param template
+     * @return
+     */
     public byte[] write(File template) {
         try (FileInputStream inputStream = new FileInputStream(template)) {
             byte[] data = write(inputStream);
@@ -83,6 +160,12 @@ public class ExcelWriter {
         }
     }
 
+    /**
+     * 写入到单元格
+     *
+     * @param workbook
+     * @param dataEntry
+     */
     private void writePoint(Workbook workbook, PointDataEntry dataEntry) {
         Point point = dataEntry.getPoint();
         Object data = dataEntry.getData();
@@ -99,23 +182,29 @@ public class ExcelWriter {
         ExcelHelper.setCellValue(cell, data);
     }
 
+    /**
+     * 写入到表格
+     *
+     * @param workbook
+     * @param dataEntry
+     */
     private void writeTable(Workbook workbook, TableDataEntry dataEntry) {
         Table table = dataEntry.getTable();
-        Collection<Map<String, Object>> data = dataEntry.getData();
+        List<Map<String, Object>> data = dataEntry.getData();
         if (Objects.isNull(data) || data.isEmpty()) {
             log.warn("表格 -> {} 设置的数据为空", table.getDataKey());
             return;
         }
 
         Sheet sheet = findSheet(workbook, table);
-        List<Map<String, Object>> dataList = List.class.isAssignableFrom(data.getClass()) ? (List<Map<String, Object>>) data : new ArrayList<>(data);
-        int dataSize = dataList.size();
+        int dataSize = data.size();
         Integer startRowNumber = table.getStartRow();
-        AtomicBoolean shiftRows = new AtomicBoolean(false);
+        AtomicBoolean moved = new AtomicBoolean(false);
+        AtomicInteger moveRows = new AtomicInteger(0);
 
         IntStream.range(startRowNumber, dataSize).forEach(i -> {
             Row row = sheet.getRow(i);
-            Map<String, Object> rowData = dataList.get(i - startRowNumber);
+            Map<String, Object> rowData = data.get(i - startRowNumber);
             // 空行处理
             if (Objects.isNull(row)) {
                 Row createdRow = sheet.createRow(i);
@@ -128,13 +217,14 @@ public class ExcelWriter {
             }
 
             // 非空行，需要判断是否需要进行移动（判断依据：行的单元格已有数据，并且包含在需要填充数据的单元格）
-            if (!shiftRows.get() && needShiftRow(row, table)) {
+            if (!moved.get() && needShiftRow(row, table)) {
                 sheet.shiftRows(i, sheet.getLastRowNum(), dataSize - i);
-                shiftRows.set(true);
+                moveRows.addAndGet(dataSize - i);
+                moved.set(true);
             }
 
             // 已移动行，移动开始 到 移动结束之间的行都是空行，需要创建
-            if (shiftRows.get()) {
+            if (moved.get()) {
                 Row createdRow = sheet.createRow(i);
                 ExcelHelper.copyRow(sheet.getRow(i - 1), createdRow);
                 writeRow(createdRow, table, rowData);
@@ -145,10 +235,18 @@ public class ExcelWriter {
             writeRow(row, table, rowData);
         });
 
-        // 合并单元格处理 TableCellMergeStrategy
-        sheet.addMergedRegion(new CellRangeAddress(3, 3, 1, 3));
-        sheet.addMergedRegion(new CellRangeAddress(1, 2, 2, 2));
-
+        // 合并单元格处理
+        List<TableCellMergeRange> tableCellMergeRanges = tableCellMergeRangesMap.get(table);
+        if (Objects.nonNull(tableCellMergeRanges) && !tableCellMergeRanges.isEmpty()) {
+            Map<String, Integer> dataKeyWithColumnNumberMap = table.getDataKeyWithColumnNumberMap();
+            tableCellMergeRanges.forEach(item -> {
+                int firstRowNumber = item.getStartRowDataIndex() + startRowNumber + moveRows.get();
+                int lastRowNumber = firstRowNumber + item.getEndRowDataIndex();
+                Integer firstColumnNumber = dataKeyWithColumnNumberMap.get(item.getStartColumnDataKey());
+                Integer lastColumnNumber = dataKeyWithColumnNumberMap.get(item.getEndColumnDataKey());
+                sheet.addMergedRegion(new CellRangeAddress(firstRowNumber, lastRowNumber, firstColumnNumber, lastColumnNumber));
+            });
+        }
     }
 
     private boolean needShiftRow(Row row, Table table) {
@@ -184,7 +282,7 @@ public class ExcelWriter {
     @AllArgsConstructor
     static class TableDataEntry {
         private Table table;
-        private Collection<Map<String, Object>> data;
+        private List<Map<String, Object>> data;
 
     }
 
