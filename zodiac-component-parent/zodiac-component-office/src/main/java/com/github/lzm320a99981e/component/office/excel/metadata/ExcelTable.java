0056e0e3c934bdfd2177ec6847453a28018c6539
zhangguangyong <1243610991@qq.com>
2019-05-27 20:13:47 +0800

    1. office-excel-注解表头配置-02

--- zodiac-component-parent/zodiac-component-office/src/main/java/com/github/lzm320a99981e/component/office/excel/metadata/ExcelTable.java
+++ zodiac-component-parent/zodiac-component-office/src/main/java/com/github/lzm320a99981e/component/office/excel/metadata/ExcelTable.java
@@ -29,4 +29,11 @@ public @interface ExcelTable {
      * @return
      */
     int[] limit();
+
+    /**
+     * 固定行数，一般用于指定表头的行数（暂时用在读取excel的时候）
+     *
+     * @return
+     */
+    int fixedRows() default 1;
 }
